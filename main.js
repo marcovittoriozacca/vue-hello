150bad9a38e8920edce25a44b145a709252a04ba
marcovittoriozacca <zaccamarco01@gmail.com>
2024-01-25 13:53:28 +0100

    feat: Random img

--- main.js
+++ main.js
@@ -4,7 +4,13 @@ createApp({
 data() {
     return {
         title: 'Primo esercizio con vue3',
-        image: 'https://picsum.photos/400/400',
+        image: 'https://picsum.photos/id/1/400/400',
     }
-}
+},
+    methods: {
+        imgChange(){
+            let randomImg = Math.floor (Math.random()*(100-1 + 1)+1);
+            this.image = `https://picsum.photos/id/${randomImg}/400/400`
+        }
+    }
 }).mount('#app')
