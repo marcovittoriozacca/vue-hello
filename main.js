ffd55f54f5399161bee623635e31e73da2c8745d
marcovittoriozacca <zaccamarco01@gmail.com>
2024-01-25 13:18:03 +0100

    feat: Title + bonus img

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
 createApp({
 data() {
     return {
-    
+        title: 'Primo esercizio con vue3',
+        image: 'https://picsum.photos/400/400',
     }
 }
 }).mount('#app')
